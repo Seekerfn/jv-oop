0d1e1ae408bb2d324cd0d5c57347adcbebfe7e1a
Zarytskyi Oleksandr <pfantom180@gmail.com>
2026-05-11 16:51:55 +0300

    Updated task2

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,4 +2,17 @@ package core.basesyntax;
 
 public class MainApp {
 
+    public static void main(String[] args) {
+    
+       Machine[] array = new Machine[] {new Truck(), new Bulldozer(), new Excavator()};
+       
+       for(Machine machine : array){
+        machine.doWork();
+        machine.stopWork();
+       }
+        
+    }
+    
+
+
 }
